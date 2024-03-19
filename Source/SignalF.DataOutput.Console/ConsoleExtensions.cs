16d2c8ebdc2b66a2aff0e759ae1be102f83bde67
Olaf Meyer <olaf.meyer@e-ypsilon.d>
2024-03-19 18:38:08 +0100

    Saved some work.

---
+++ Source/SignalF.DataOutput.Console/ConsoleExtensions.cs
@@ -0,0 +1,34 @@
+﻿using SignalF.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalF.Configuration.DataOutput;
+using Iot.Device.CpuTemperature;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SignalF.DataOutput.Console
+{
+    public static class ConsoleExtensions
+    {
+        private const string ConsoleTemplateName = "ConsoleTemplate";
+        private const string ConsoleDefinitionName = "ConsoleDefinition";
+        private const string ConsoleDefaultName = "Console";
+
+        public static IServiceCollection AddConsole(this IServiceCollection services)
+        {
+            return services.AddTransient<DataOutputSenderConsole>();
+        }
+
+
+        public static ISignalFConfiguration AddDataOutputSenderConsole(this ISignalFConfiguration configuration
+            , Action<IDataOutputSenderConfigurationBuilder<DataOutputSenderConsoleOptions>> builder)
+        {
+            return configuration
+                .AddDataOutputSenderConfiguration<IDataOutputSenderConfigurationBuilder, DataOutputSenderConsoleOptions,
+                    IDataOutputSenderConsole>(builder);
+        }
+
+    }
+}
